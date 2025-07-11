da00903802cc81aed6850beb79fc0a67e9d21c33
Kris <blackphoenix134@mail.ru>
2025-07-12 00:33:00 +0300

    feat(data): update CourseDao to use CourseEntity Replace Course with CourseEntity in CourseDao methods and ensure SQL query references correct table and field names.

--- app/src/main/java/com/example/thousandscourses/data/local/CourseDao.kt
+++ app/src/main/java/com/example/thousandscourses/data/local/CourseDao.kt
@@ -4,14 +4,14 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.thousandscourses.domain.model.Course
+import com.example.thousandscourses.data.local.model.CourseEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CourseDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertCourse(course: Course)
+    suspend fun insertCourse(course: CourseEntity)
 
-    @Query("SELECT * FROM course WHERE haslike = 1")
-    fun getFavoriteCourses(): Flow<List<Course>>
+    @Query("SELECT * FROM course WHERE hasLike = 1")
+    fun getFavoriteCourses(): Flow<List<CourseEntity>>
 }
